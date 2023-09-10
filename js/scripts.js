6b27b0fc81a9fa7ee4c1e1fe78a03413940443c9
clinton okpere <clintonokopere99@gmail.com>
2023-09-10 10:00:50 +0100

    changes made

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+const currentUTCTime = document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  );
+  const currentDayOfTheWeek = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+
+  // current date
+  const currentDate = new Date();
+
+  // array of day by names
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  //current day of the week
+  const currentDayIndex = currentDate.getDay();
+
+  //name of the current day
+  const currentDayName = daysOfWeek[currentDayIndex];
+
+  currentDayOfTheWeek.innerHTML = "Today is : " + currentDayName;
+
+  // current UTC time in milliseconds
+  const currentUTCTimeMillis = Date.now();
+
+  currentUTCTime.innerHTML =
+    "Time in Milliseconds : " + currentUTCTimeMillis;
